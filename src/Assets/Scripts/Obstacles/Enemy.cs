7f0569e334b93ce77c88ca7ca2db426df97f0b28
Aiz <dev@aiz.moe>
2021-04-27 19:39:17 +0200

    Try to make everything work
    shooting doesnt work that well. UI should now scale. tried to clean some code up.

--- src/Assets/Scripts/Obstacles/Enemy.cs
+++ src/Assets/Scripts/Obstacles/Enemy.cs
@@ -35,7 +35,7 @@ public class Enemy : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             //Instantiate(effect, transform.position, Quaternion.identity);
-            GameController.gameControllerInstance.DecrementHealth(damage);
+            GameController.instance.DecrementHealth(damage);
             //Destroy(gameObject);
         }
     }
