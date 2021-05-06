16cb28f49ad424a6e51f5254ec4804ec57381848
Aiz <dev@aiz.moe>
2021-05-06 17:22:59 +0200

    PufferFish Animation Script

--- src/Assets/Scripts/Obstacles/Pufferfish_Movement.cs
+++ src/Assets/Scripts/Obstacles/Pufferfish_Movement.cs
@@ -2,24 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pufferfish_Movement : MonoBehaviour 
+public class Pufferfish_Movement : MonoBehaviour
 {
     [SerializeField]
-    private float speedMultiplier = 1.5f;
+    private float puffAt = 3;
+
     [SerializeField]
     private Animator animator;
-    [SerializeField]
-    private Rigidbody2D rb;
-
-    private GameController gameController;
 
     private void Start()
     {
-        gameController = GameController.instance;
+        animator.SetBool("Puff", false);
     }
 
     private void Update()
     {
-        rb.velocity = gameController.GetDirection() * gameController.GetSpeed() * speedMultiplier;
+        if(transform.position.y <= puffAt) animator.SetBool("Puff", true);
     }
 }
