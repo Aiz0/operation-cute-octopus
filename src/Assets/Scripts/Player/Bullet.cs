c62b3ab652906e41eca2b799657a7d7f9bb73666
MVBNV700 <vikstrom18noah@gmail.com>
2021-05-06 11:58:00 +0200

    Update Bullet.cs

--- src/Assets/Scripts/Player/Bullet.cs
+++ src/Assets/Scripts/Player/Bullet.cs
@@ -4,30 +4,31 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField]
-    private float speed = 20f;
-    [SerializeField]
-    private int damage = 1;
-    [SerializeField]
-    private Rigidbody2D rb;
+    public float speed = 20f;
+    public int damage = 1;
+    public Rigidbody2D rb;
 
+    public string tagToIgnore = "Player";
+    // Start is called before the first frame update
     void Start()
     {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), GetComponent<Collider2D>());
         rb.velocity = transform.right * speed;
+        Destroy(gameObject, 2);
     }
 
     private void OnTriggerEnter2D(Collider2D hitInfo)
     {
-        Shootable shootable = hitInfo.GetComponent<Shootable>();
-        if (shootable != null) {
+        EnemyFollow enemy = hitInfo.GetComponent<EnemyFollow>();
+        if (enemy != null)
+        {
+            enemy.IsShot();
             Destroy(gameObject);
-<<<<<<< HEAD
         }else if (hitInfo.CompareTag("Obstacle"))
         {
             Destroy(gameObject);
-=======
->>>>>>> fe431a852313d32ba5cb010a7c77af8e99643513
         }
     }
-
+   
 }
