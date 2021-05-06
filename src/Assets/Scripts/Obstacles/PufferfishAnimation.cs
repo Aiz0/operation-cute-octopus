ef2c4198104fc61dbc729fe6114d0802e31837c7
Aiz <dev@aiz.moe>
2021-05-06 17:34:42 +0200

    Animate Pufferfish

--- src/Assets/Scripts/Obstacles/PufferfishAnimation.cs
+++ src/Assets/Scripts/Obstacles/PufferfishAnimation.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pufferfish_Movement : MonoBehaviour
+[RequireComponent(typeof(Animator))]
+public class PufferfishAnimation : MonoBehaviour
 {
     [SerializeField]
     private float puffAt = 3;
 
-    [SerializeField]
     private Animator animator;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
 
     private void Start()
     {
