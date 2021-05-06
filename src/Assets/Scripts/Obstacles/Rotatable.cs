cd8c1d03925a3f8d01a920289e64a9a4c69b703d
Aiz <dev@aiz.moe>
2021-05-06 16:55:42 +0200

    Add random starting rotation

--- src/Assets/Scripts/Obstacles/Rotatable.cs
+++ src/Assets/Scripts/Obstacles/Rotatable.cs
@@ -9,11 +9,21 @@ public class Rotatable : MonoBehaviour
     private int maxRotateSpeed;
     [SerializeField]
     private int minRotateSpeed;
+    [SerializeField]
+    private bool randomStartRotation = true;
+
     private int rotationSpeed;
 
     private void Awake()
     {
         rotationSpeed = Random.Range(minRotateSpeed, maxRotateSpeed) * (Random.Range(0,2)*2-1);
+    }
+
+    private void Start() {
+        if (randomStartRotation) {
+            float startRotation = Random.Range(0,360);
+            transform.rotation = Quaternion.Euler(0, 0, startRotation);
+        }
     }
 
     private void Update()
