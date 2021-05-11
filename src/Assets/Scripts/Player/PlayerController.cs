6388f874c3a31f1015802053a1b01d52e471c303
Aiz <dev@aiz.moe>
2021-05-11 18:23:42 +0200

    Only allow movement while game is running

--- src/Assets/Scripts/Player/PlayerController.cs
+++ src/Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,10 +10,12 @@ public class PlayerController : MonoBehaviour
     private PlayerData pData;
     private Rigidbody2D rb2D;
     private Vector2 axis;
+    private GameController gameController;
 
     private void Awake() {
         pData = GetComponent<PlayerData>();
         rb2D = GetComponent<Rigidbody2D>();
+        gameController = GameController.instance;
 
         if (AttitudeSensor.current != null) {
             InputSystem.EnableDevice(AttitudeSensor.current);
@@ -32,7 +33,10 @@ public class PlayerController : MonoBehaviour
         }else{
             direction = axis.x;
         }
-        Move(direction);
+        if(gameController.GameRunning){
+
+            Move(direction);
+        }
     }
 
     public void OnMove(InputValue input){
