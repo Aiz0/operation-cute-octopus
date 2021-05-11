669cd59b4bce2081d47379416ad9cdec84041266
Aiz <dev@aiz.moe>
2021-05-11 18:31:28 +0200

    Set velocity to 0 on death

--- src/Assets/Scripts/Player/PlayerDie.cs
+++ src/Assets/Scripts/Player/PlayerDie.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerDie : MonoBehaviour
 {
     private GameController gameController;
     private Animator animator;
+    private Rigidbody2D rb;
 
     private void Awake() {
         gameController = GameController.instance;
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnEnable() {
@@ -24,5 +27,6 @@ public class PlayerDie : MonoBehaviour
     public void Die(){
         animator.SetBool("Dead", true);
         Destroy(gameObject,1);
+        rb.velocity = new Vector2(0,0);
     }
 }
