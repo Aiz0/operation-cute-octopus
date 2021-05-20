6ffefe2043765808fb125d5bfdfc7feed0a1d992
MVBNV700 <vikstrom18noah@gmail.com>
2021-05-20 17:23:31 +0200

    Blåsfisk animation
    Lagt till simanimationen för blåsfisken.
    
    Problemet nu:
    - Fel meddelande på AnimationEvent
    - Blåsfisken far i sidled neråt

--- src/Assets/Scripts/Obstacles/PufferfishAnimation.cs
+++ src/Assets/Scripts/Obstacles/PufferfishAnimation.cs
@@ -12,10 +12,6 @@ public class PufferfishAnimation : MonoBehaviour
 
     private void Awake() {
         animator = GetComponent<Animator>();
-    }
-
-    private void Start()
-    {
         animator.SetBool("Puff", false);
     }
 
