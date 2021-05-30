8f90132ea7b289e055d8575c2460a24f7ae2da5b
Aiz <66974576+Aiz0@users.noreply.github.com>
2021-05-30 16:22:43 +0000

    Powerups (#68)
    * pull request 61 merged to main without conflicts_
    
    powerups, sounds, good stuff
    
    * Fix merge issue
    
    * fix merge issues and turn some objects into prefabs
    
    * Replace old sound manager with new one
    
    Replace the old sound manager with the prefab one. Nothing major
    
    Co-authored-by: toso6538 <toso6538@student.su.se>
    Co-authored-by: thangn2703 <82639562+thangn2703@users.noreply.github.com>

--- src/Assets/Scripts/Audio/SoundManager.cs
+++ src/Assets/Scripts/Audio/SoundManager.cs
@@ -8,6 +8,8 @@ public class SoundManager : MonoBehaviour
     private AudioSource audioSource;
     private AudioClip[] inkSounds;
     private AudioClip[] destroySounds;
+    private AudioClip[] starSounds;
+    private AudioClip[] rocketEndSounds;
     private int randomNumber;
 
     void Start()
@@ -16,19 +18,33 @@ public class SoundManager : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
         inkSounds = Resources.LoadAll<AudioClip>("InkSounds");
         destroySounds = Resources.LoadAll<AudioClip>("DestroySounds");
+        starSounds = Resources.LoadAll<AudioClip>("StarSounds");
+        rocketEndSounds = Resources.LoadAll<AudioClip>("RocketEndSounds");
     }
 
     public void PlayInkSound()
     {
         randomNumber = Random.Range(0, 4);
         audioSource.PlayOneShot(inkSounds[randomNumber]);
-     }
+    }
 
 
     public void PlayDestroySound()
     {
         randomNumber = Random.Range(0, 2);
         audioSource.PlayOneShot(destroySounds[randomNumber]);
+    }
+
+    public void PlayStarSound()
+    {
+        randomNumber = Random.Range(0, 1); //bara ett Star Sound, men kodat för att kunna lägga till fler
+        audioSource.PlayOneShot(starSounds[randomNumber]);
+    }
+
+    public void PlayRocketEndSound()
+    {
+        randomNumber = Random.Range(0, 1); //bara ett Rocket End Sound, men kodat för att kunna lägga till fler
+        audioSource.PlayOneShot(rocketEndSounds[randomNumber]);
     }
 
 }
